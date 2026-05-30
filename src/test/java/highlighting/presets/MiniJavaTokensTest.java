f534fb6f56aae3bfbaeb92cffeaf54937155d671
nibbu2822 <m.wesseler1998@gmail.com>
2026-05-31 01:57:30 +0200

    add ci and tests

---
+++ src/test/java/highlighting/presets/MiniJavaTokensTest.java
@@ -0,0 +1,7 @@
+package highlighting.presets;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MiniJavaTokensTest {
+
+}
---
+++ src/test/java/highlighting/regex/RegexHighlighterTest.java
@@ -0,0 +1,16 @@
+package highlighting.regex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegexHighlighterTest {
+
+    @Test
+    void collectMatches() {
+    }
+
+    @Test
+    void resolveConflicts() {
+    }
+}
